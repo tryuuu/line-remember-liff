016810851e75f8a092d314de8de9df1e3ff5a33b
竜鶴吉 <ryutsuruyoshi@ryusMacBook-Air.local>
2023-12-06 16:52:42 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-if="userData && userData.length">
+      <div v-for="(item, index) in userData" :key="index" class="message-item">
+        <img v-if="isImageUrl(item)" :src="item" alt="Image" class="message-image" />
+        <pre v-else class="message-code"><code>{{ item }}</code></pre>
+      </div>
+    </div>
+    <div v-else>
+      データをロード中...
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<style>
+.message-item {
+  max-width: 90%;
+  margin: 10px auto;
+}
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+.message-image {
+  width: 100%;
+  height: auto;
+  border-radius: 5px;
+}
+
+.message-code {
+  white-space: pre-wrap;
+  background-color: #f5f5f5;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 10px;
+  overflow-x: auto;
+}
+
+@media (max-width: 600px) {
+  .message-item {
+    max-width: 100%;
   }
 }
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
+
+
+<script>
+import { defineComponent, ref, onMounted } from 'vue';
+import liff from '@line/liff';
+import axios from 'axios';
+
+export default defineComponent({
+  setup() {
+    const userData = ref(null);
+
+    function isImageUrl(url) {
+      return /\.(jpg|jpeg|png|gif)$/i.test(url);
+    }
+
+    async function initializeLiff() {
+      try {
+        await liff.init({ liffId: "2002041664-Jz0GGDWZ" });
+        if (liff.isLoggedIn()) {
+          const profile = await liff.getProfile();
+          return profile.userId;
+        } else {
+          liff.login();
+        }
+      } catch (error) {
+        console.error("LIFF Initialization failed", error);
+      }
+    }
+
+    async function fetchUserData(userId) {
+      try {
+        const response = await axios.post('https://nif4ujmi72rexthm55wuu2tyom0pnbbc.lambda-url.ap-northeast-1.on.aws/', {
+          user_id: userId
+        });
+        return response.data;
+      } catch (error) {
+        console.error("Error fetching data from Lambda", error);
+      }
+    }
+    async function loadData() {
+      const userId = await initializeLiff();
+      if (userId) {
+        userData.value = await fetchUserData(userId);
+      }
+    }
+
+    onMounted(loadData);
+
+    return { userData,isImageUrl };
+  },
+});
+</script>
